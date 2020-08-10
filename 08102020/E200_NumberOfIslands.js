12ab511e3dbc981606530ed799a823cab9d0c99b
Jing Chen <jchen0506@gmail.com>
2020-08-10 12:03:20 -0500

    number of islands finished

--- 08102020/E200_NumberOfIslands.js
+++ 08102020/E200_NumberOfIslands.js
@@ -20,3 +20,45 @@ Input: grid = [
 ]
 Output: 3
 */
+
+var numberOfIslands = (grid) => {
+  var result = 0;
+  var m = grid.length;
+  var n = grid[0].length;
+  for (var i = 0; i < m; i++) {
+    for (var j = 0; j < n; j++) {
+      if (grid[i][j] === '1') {
+        result++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  return result;
+};
+
+var dfs = (grid, r, c) => {
+  if (
+    r < 0 ||
+    c < 0 ||
+    r >= grid.length ||
+    c >= grid[0].length ||
+    grid[r][c] === '0'
+  ) {
+    return;
+  }
+  grid[r][c] = '0';
+  dfs(grid, r, c + 1);
+  dfs(grid, r + 1, c);
+  dfs(grid, r, c - 1);
+  dfs(grid, r - 1, c);
+};
+
+//test case
+
+grid = [
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1'],
+];
+console.log(numberOfIslands(grid));
