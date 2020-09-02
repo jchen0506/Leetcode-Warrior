3a5929fb5d35eb53aca0e079fc9bf5d678c6bbf8
Jing Chen <jchen0506@gmail.com>
2020-09-02 11:34:02 -0500

    daily challenge 0902

---
+++ dailyChalleng0902ContainsDuplicate3.js
@@ -0,0 +1,33 @@
+/*
+Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+Example 3:
+
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+
+Hint1:
+Time complexity O(n logk) - This will give an indication that sorting is involved for k elements.
+
+Hint2:
+Use already existing state to evaluate next state - Like, a set of k sorted numbers are only needed to be tracked. When we are processing the next number in array, then we can utilize the existing sorted state and it is not necessary to sort next overlapping set of k numbers again.
+*/
+var containsNearbyAlmostDuplicate = function (nums, k, t) {
+  for (let i = 1; i <= k; i++) {
+    for (let j = 0; j < nums.length - i; j++) {
+      let curWindow = Math.abs(nums[j + i] - nums[j]);
+      if (curWindow <= t) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
