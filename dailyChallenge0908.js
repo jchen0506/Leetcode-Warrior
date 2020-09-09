289c57e8e5a7bb922b1c5cf89c1539fafe34d8ef
Jing Chen <jchen0506@gmail.com>
2020-09-09 12:52:56 -0500

    daily challenge 0909 0908

---
+++ dailyChallenge0908.js
@@ -0,0 +1,39 @@
+/*
+Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+Note:
+
+The number of nodes in the tree is between 1 and 1000.
+node.val is 0 or 1.
+The answer will not exceed 2^31 - 1.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+
+    var dfs = function(root,path) {
+      if (root === null) return 0;
+      path += root.val;
+      if (root.left === null && root.right === null) {
+        return parseInt(path, 2);
+      }
+      return dfs(root.left,path) +dfs(root.right,path);
+    }
+
+    return dfs(root,'')
+};
