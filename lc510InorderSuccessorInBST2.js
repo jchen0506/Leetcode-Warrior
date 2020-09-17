fa874d3724301fae5295014f51e7a39fd08cf95d
Jing Chen <jchen0506@gmail.com>
2020-09-17 18:02:40 -0500

    previous practice

---
+++ lc510InorderSuccessorInBST2.js
@@ -0,0 +1,46 @@
+/*
+Given a node in a binary search tree, find the in-order successor of that node in the BST.
+
+If that node has no in-order successor, return null.
+
+The successor of a node is the node with the smallest key greater than node.val.
+
+You will have direct access to the node but not to the root of the tree. Each node will have a reference to its parent node. Below is the definition for Node:
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+
+
+Follow up:
+
+Could you solve it without looking up any of the node's values?
+*/
+/**
+ * // Definition for a Node.
+ * function Node(val) {
+ *    this.val = val;
+ *    this.left = null;
+ *    this.right = null;
+ *    this.parent = null;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var inorderSuccessor = function (node) {
+  if (node.right) {
+    node = node.right;
+    while (node.left) node = node.left;
+    return node;
+  }
+
+  while (node.parent && node === node.parent.right) node = node.parent;
+
+  return node.parent;
+};
