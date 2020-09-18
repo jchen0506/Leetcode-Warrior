cf631079b012116836f84f5e9745c6db1dbd6605
Jing Chen <jchen0506@gmail.com>
2020-09-18 18:35:52 -0500

    modification

--- dailyChallenge0918.js
+++ dailyChallenge0918.js
@@ -22,12 +22,19 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.s
  * @param {number[]} prices
  * @return {number}
  */
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
 var maxProfit = function (prices) {
-  let profit = 0;
-  for (let i = 0; i < prices.length - 1; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      profit = Math.max(profit, prices[j] - prices[i]);
+  let minprice = prices[0];
+  let maxprofit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minprice) {
+      minprice = prices[i];
+    } else if (prices[i] - minprice > maxprofit) {
+      maxprofit = prices[i] - minprice;
     }
   }
-  return profit;
+  return maxprofit;
 };
